9cab716a27fd6be4742fb8f86be3a193b1e741e9
Martin Holý <martin.holy.022@pslib.cz>
2024-09-04 21:17:24 +0200

    Přidejte soubory projektu.

---
+++ APIcontroller/Controllers/People.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIcontroller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class People : ControllerBase
+    {
+        [HttpGet("get-people")]
+        public object Get()
+        {
+            Person iveta = new Person(1, "Iveta", "Nová", "iveta.nova@seznam.cz", 758471241);
+            Person martin = new Person(2, "Martin", "Nový", "martin.novy69@pslib.cz", 731371311);
+            Person ivan = new Person(3, "Ivan", "Dvořák", "ivandvorakhaf@gmail.com", 789523641);
+
+            object[] people = [iveta, martin, ivan];
+            
+
+            return people;
+        }
+
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string Email { get; set; }
+            public int Phone { get; set; }
+
+            public Person(int id, string name, string surname, string email, int phone)
+            {
+                this.Id = id;
+                this.Name = name;
+                this.Surname = surname;
+                this.Email = email;
+                this.Phone = phone;
+            }
+
+        }
+    }
+}
---
+++ APIcontroller/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace APIcontroller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public int Get()
+        {
+            return Random.Shared.Next(0, 100);
+        }
+        [HttpGet("max")]
+        public int GetMax()
+        {
+            return Random.Shared.Next(10, 30);
+        }
+
+        [HttpGet("name")]
+        public Date GetName()
+        {
+            Date x = new Date { FirstName = "Alice" };
+            return x;
+        }
+        [HttpGet("err")]
+        public IActionResult GetErr()
+        {
+            Date x = new Date { FirstName = "Alice" };
+            return StatusCode(413, "whats happening");
+        }
+        public class Date
+        {
+            public string FirstName { get; set; }
+        }
+    }
+}
